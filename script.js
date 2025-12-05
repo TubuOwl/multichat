0d513f83d10a344a18920c95d62750544738a882
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-12-05 22:50:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("prevBtn").style.display = "none";
     document.getElementById("nextBtn").style.display = "none";
   }
-  document.querySelectorAll(".topbar").forEach(el => el.remove());
-  document.querySelectorAll("script[src*='revcontent'], script[src*='ad-score']").forEach(s => s.remove());
 
+setInterval(() => {
+  const tb = document.querySelector(".topbar");
+  if (tb) tb.remove();
+}, 300);
 });
+
