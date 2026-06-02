c1b5f1b2983824027f8d6c6d1215b5c518c35da7
Tubu <74395702+TubuOwl@users.noreply.github.com>
2026-06-03 01:52:48 +0700

    Use environment variable for server port
    Updated server to use PORT environment variable for configuration.

--- server.py
+++ server.py
@@ -89,4 +89,6 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    # Render inject PORT via env variable
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
